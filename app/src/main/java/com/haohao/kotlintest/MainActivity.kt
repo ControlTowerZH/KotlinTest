e7d2f487e7d58ff1e4e2a5dc54f7d4ffe2573c74
zhcontroltower <1161178411@qq.com>
2020-02-03 17:45:00 +0800

    Initial commit

---
+++ app/src/main/java/com/haohao/kotlintest/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.haohao.kotlintest
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.haohao.kotlintest.util.ToastUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        tv_main.setOnClickListener {
+            ToastUtils.showShort(baseContext,"ListMain")
+        }
+
+        tv_test1.setOnClickListener {
+            ToastUtils.showShort(baseContext,"tv_test1")
+        }
+        tv_test2.setOnClickListener {
+            ToastUtils.showShort(baseContext,"tv_test2")
+        }
+        tv_test3.setOnClickListener {
+            ToastUtils.showShort(baseContext,"tv_test3")
+        }
+    }
+}
---
+++ app/src/main/java/com/haohao/kotlintest/MainListActivity.kt
@@ -0,0 +1,12 @@
+package com.haohao.kotlintest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_list)
+    }
+}
