567ee531cc3fc46841289b10995b2e9861bd9c1d
zhcontroltower <1161178411@qq.com>
2020-07-05 23:10:18 +0800

    通知栏沉浸

--- app/src/main/java/com/haohao/kotlintest/function/ContainsActivity.java
+++ app/src/main/java/com/haohao/kotlintest/function/ContainsActivity.java
@@ -2,8 +2,11 @@ package com.haohao.kotlintest.function;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 
 import com.haohao.kotlintest.R;
 
@@ -16,13 +19,13 @@ import androidx.fragment.app.FragmentTransaction;
  * 容器Activity 包裹Fragment ,最好再实现一下 模仿Activity任务栈
  */
 public class ContainsActivity extends AppCompatActivity {
-    public static final String FRAGMENT_OK_HTTP= "okHttpFragment";
-    public static final String FRAGMENT_DESIGN= "designFragment";
-    public static final String FRAGMENT_TYPE= "Fragment";
+    public static final String FRAGMENT_OK_HTTP = "okHttpFragment";
+    public static final String FRAGMENT_DESIGN = "designFragment";
+    public static final String FRAGMENT_TYPE = "Fragment";
 
-    public static void start(Context context,String type){
-        Intent intent =new Intent(context,ContainsActivity.class);
-        intent.putExtra(FRAGMENT_TYPE,type);
+    public static void start(Context context, String type) {
+        Intent intent = new Intent(context, ContainsActivity.class);
+        intent.putExtra(FRAGMENT_TYPE, type);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
@@ -30,9 +33,15 @@ public class ContainsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contains);
-
         String fragmentType = getIntent().getStringExtra(FRAGMENT_TYPE);
+        if (fragmentType != null && fragmentType.equals(FRAGMENT_DESIGN)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            }
+        }
+        setContentView(R.layout.activity_contains);
         if (!TextUtils.isEmpty(fragmentType)) {
             switch (fragmentType) {
                 case FRAGMENT_OK_HTTP:
@@ -47,10 +56,10 @@ public class ContainsActivity extends AppCompatActivity {
 
     }
 
-    private void replaceFragment(Fragment fragment){
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.fl_fragment,fragment);
+        transaction.replace(R.id.fl_fragment, fragment);
         transaction.commit();
     }
 }
--- app/src/main/java/com/haohao/kotlintest/function/DesignFragment.kt
+++ app/src/main/java/com/haohao/kotlintest/function/DesignFragment.kt
@@ -1,12 +1,11 @@
 package com.haohao.kotlintest.function
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
-
 import com.haohao.kotlintest.R
 import kotlinx.android.synthetic.main.fragment_design.*
 
@@ -23,7 +22,6 @@ class DesignFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_design, container, false)
     }
 
