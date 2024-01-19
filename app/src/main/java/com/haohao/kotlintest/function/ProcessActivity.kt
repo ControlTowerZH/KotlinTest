eb1f73c5e6a43f9d7dcddc8d6ea513b6a99c7d5a
hao.zhao <hao.zhao@megatronix.co>
2024-01-19 22:53:36 +0800

    process test

--- app/src/main/java/com/haohao/kotlintest/function/ProcessActivity.kt
+++ app/src/main/java/com/haohao/kotlintest/function/ProcessActivity.kt
@@ -1,7 +1,9 @@
 package com.haohao.kotlintest.function
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.haohao.kotlintest.R
 import com.haohao.kotlintest.function.service.ProcessService
@@ -15,12 +17,18 @@ class ProcessActivity : AppCompatActivity() {
         Timber.d("ProcessActivity Create")
     }
 
+    @SuppressLint("LogNotTimber")
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         //开启一个新的服务，此服务为单独的进程
         //开启新进程，application需要重新启动，可以根据 进程名配置
+        Log.d(TAG,"start ProcessService")
         val intent = Intent(this, ProcessService::class.java)
         intent.action = "android.intent.action.RESPOND_VIA_MESSAGE";
         startService(intent)
+    }
+
+    companion object {
+        private const val TAG = "ProcessActivity"
     }
 }
